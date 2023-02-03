5b253b41ab7b18fcfc6b97f88786d92b8fb40a70
Rajvir Chalthanwala <105453375+RajvirChalthanwala@users.noreply.github.com>
2023-02-03 18:19:07 -0500

    solve error

--- R_Chalthanwala_Movies/src/app/content-card/content-card.component.ts
+++ R_Chalthanwala_Movies/src/app/content-card/content-card.component.ts
@@ -1,39 +1,54 @@
 import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { ContentList } from '../helper-files/content-list';
+
 @Component({
   selector: 'app-content-card',
-  template: `
-    <div class="card">
-    <h3>Content List</h3>
-      <ul>
-          <li *ngFor="let item of contentList.getItems()">{{item}}</li>
-      </ul>
-    </div>
-  `
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-  private contentList: ContentList;
+  myContentList: ContentList = new ContentList();
+
+  contentItem1: Content = {
+    id: 0,
+    title: 'mySampleTitle',
+    description: 'This is a description',
+    creator: 'Creator'
+  }
+  contentItem2: Content = {
+    id: 0,
+    title: 'mySampleTitle2',
+    description: 'This is a description2',
+    creator: 'Creator2'
+  }
+  contentItem3: Content = {
+    id: 0,
+    title: 'mySampleTitle3',
+    description: 'This is a description4',
+    creator: 'Creator2'
+  }
+  contentItem4: Content = {
+    id: 0,
+    title: 'mySampleTitle4',
+    description: 'This is a description4',
+    creator: 'Creator'
+  }
 
   constructor() {
-    this.contentList = new ContentList();
-    this.contentList.add("Item 1");
-    this.contentList.add('Item 2');
-    this.contentList.add('Item 3');
+    this.myContentList.addFunction(this.contentItem1);
+    this.myContentList.addFunction(this.contentItem2);
+    this.myContentList.addFunction(this.contentItem3);
+    this.myContentList.addFunction(this.contentItem4);
   }
-  getContentList() {
-    return this.contentList;
-  }
-}
 
-const contentCard = new ContentCardComponent();
-console.log(contentCard.getContentList().getItems());
+  displayMyIndex(index: number) {
+    let myItems = this.myContentList.getItems();
 
-export class ContentList {
-  private items: string[] = [];
-
-  add(item: string) {
-    this.items.push(item);
-  }
-  getItems() {
-    return this.items;
+    if (myItems.length > 0 && index > 0 && index < myItems.length) {
+      return this.myContentList.printIndex(index);
+    } else {
+      alert("NO THAT IS BAD");
+    }
   }
 }
