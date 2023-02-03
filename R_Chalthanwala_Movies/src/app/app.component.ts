bf26e137c7e5cba3c93968db51c22bcf6bc10cf8
Rajvir Chalthanwala <105453375+RajvirChalthanwala@users.noreply.github.com>
2023-02-03 16:12:57 -0500

    Commit till Final error of Content Display on home page

--- R_Chalthanwala_Movies/src/app/app.component.ts
+++ R_Chalthanwala_Movies/src/app/app.component.ts
@@ -2,8 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
+    `
+    <div>
+<h1>Welcome to my App</h1>
+<content-card></content-card>
+</div>
+`,
   styleUrls: ['./app.component.scss']
+})
+
+@Component({
+  selector: 'content-card',
+  template: `
+    <div>
+<h3>Content List</h3>
+<ul>
+<li *ngFor="let item of contentList.getItems()">{{item}}</li>
+</ul>
+</div>
+`
 })
 export class AppComponent {
   title = 'R_Chalthanwala_Movies';
