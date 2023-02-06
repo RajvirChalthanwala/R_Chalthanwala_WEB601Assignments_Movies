8403061b01fc8e66ee51c7e538f6afdda6f14a25
Rajvir Chalthanwala <105453375+RajvirChalthanwala@users.noreply.github.com>
2023-02-06 17:08:29 -0500

    Commit Till Assignment_1

--- R_Chalthanwala_Movies/src/app/app.component.ts
+++ R_Chalthanwala_Movies/src/app/app.component.ts
@@ -2,27 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
-    `
-    <div>
-<h1>Welcome to my App</h1>
-<content-card></content-card>
-</div>
-`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-@Component({
-  selector: 'content-card',
-  template: `
-    <div>
-<h3>Content List</h3>
-<ul>
-<li *ngFor="let item of contentList.getItems()">{{item}}</li>
-</ul>
-</div>
-`
-})
 export class AppComponent {
   title = 'R_Chalthanwala_Movies';
 }
