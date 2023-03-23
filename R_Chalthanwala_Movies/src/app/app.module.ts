fff4bb49ffdbda3e3042e88e7f90b73f8b1f1b15
Rajvir Chalthanwala <105453375+RajvirChalthanwala@users.noreply.github.com>
2023-03-23 19:57:53 -0400

    Initial commit for next assignment

--- R_Chalthanwala_Movies/src/app/app.module.ts
+++ R_Chalthanwala_Movies/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentListPipe } from './content-list.pipe';
 import { FormsModule } from '@angular/forms';
+import { HoverAffectDirective } from './hover-affect.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
-    ContentListPipe
+    ContentListPipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule,
