550175ab04fde26533cce6073b2570f45e763c41
Rajvir Chalthanwala <105453375+RajvirChalthanwala@users.noreply.github.com>
2023-04-21 19:24:54 -0400

    Commit All

--- R_Chalthanwala_Movies/src/app/modify-content/modify-content.component.ts
+++ R_Chalthanwala_Movies/src/app/modify-content/modify-content.component.ts
@@ -1,5 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Content } from '../helper-files/content-interface';
+import { AddContentDialogComponent } from '../add-content-dialog/add-content-dialog.component';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-modify-content',
@@ -9,6 +12,24 @@ import { Content } from '../helper-files/content-interface';
 export class ModifyContentComponent {
   newMovie: any = {};
   @Output() addMovieEvent = new EventEmitter<Content>();
+
+  constructor(public dialog: MatDialog, private MessageService: MessageService) { }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AddContentDialogComponent, {
+      width: '60%'
+    });
+    dialogRef.afterClosed().subscribe(newMovieFromDialog => {
+      if (newMovieFromDialog) {
+        if (newMovieFromDialog.title && newMovieFromDialog.description && newMovieFromDialog.creator) {
+          this.newMovie = newMovieFromDialog;
+          this.addNewMovie();
+        } else {
+          this.MessageService.add('No content added. Required fields were empty');
+        }
+      }
+    });
+  }
 
   addNewMovie() {
     this.addMovieEvent.emit(this.newMovie);
